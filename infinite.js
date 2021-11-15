6ca483c3c7818eab1f2714391186b52011fa2d45
gaometri <37038820+gaometri@users.noreply.github.com>
2021-11-15 18:42:21 +0300

    Update infinite.js

--- infinite.js
+++ infinite.js
@@ -1,5 +1,6 @@
 var rain = document.createElement("script");
-rain.type = "text/javascript", rain.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js", rain.async = !0, rain.onload = function() {
+rain.type = "text/javascript", rain.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js", 
+rain.async = !0, rain.onload = function() {
 	! function(i) {
 		if ($("body").append("<footer>IGLO Template by igniel.com</footer>"), config.infinite_scroll) {
 			var e = i(".post-nav"),
@@ -7,9 +8,8 @@ rain.type = "text/javascript", rain.src = "https://ajax.googleapis.com/ajax/libs
 				n = !1;
 			e.find("#Blog1_blog-pager-newer-link").remove(), e.on("click", "#Blog1_blog-pager-older-link a", function() {
 				return history.pushState("", "", this.href), i.get(this.href, {}, function(l) {
-					var o = i(l).find(".hfeed").length ? i(l) : i("<div></div>");
-					t.append(o.find(".blog-posts").html()), e.html(o.find("#Blog1_blog-pager-older-link").clone()), 
-					t.find(".more-article article").append(o.find(".more-article article").clone()), window.location.href.match("/search\\?updated-max") || (document.title = o.find(".post-title").first().text(), t.append('<div class="ignielMiddleAds">' + o.find(".ignielMiddleAds").html() + "</div>"), ignielMiddleAds()), n = !1
+					var o = i(l).find(".post-outer").length ? i(l) : i("<div></div>");
+					t.append(o.find(".blog-posts").html()), e.html(o.find("#Blog1_blog-pager-older-link").clone()), t.find(".more-article columns column-2").append(o.find(".more-article columns column-2 article").clone()), window.location.href.match("/search\\?updated-max") || (document.title = o.find(".article-header").first().text(), t.append('<div class="ignielMiddleAds">' + o.find(".ignielMiddleAds").html() + "</div>"), ignielMiddleAds()), n = !1
 				}, "html"), i(this).replaceWith('<span><img src="https://4.bp.blogspot.com/-a8y2WkWKzU0/W90DTo4X29I/AAAAAAAAG2c/5FWxJt9VaYUM7Mz-bH0emW3A50lJxCltQCLcBGAs/s1600/igniel-loading.gif"/></span>'), !1
 			}), config.auto && $(window).on("scroll resize", function() {
 				!n && $(this).scrollTop() + $(this).height() >= e.offset().top && (e.find("#Blog1_blog-pager-older-link a").trigger("click"), n = !0)
