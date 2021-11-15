2c48ffcac95266d1e2b1b5984667ee3012369c5a
gaometri <37038820+gaometri@users.noreply.github.com>
2021-11-15 18:48:03 +0300

    Update infinite.js

--- infinite.js
+++ infinite.js
@@ -6,13 +6,17 @@ rain.async = !0, rain.onload = function() {
 			var e = i(".post-nav"),
 				t = i(".blog-posts"),
 				n = !1;
-			e.find("#Blog1_blog-pager-newer-link").remove(), e.on("click", "#Blog1_blog-pager-older-link a", function() {
-				return history.pushState("", "", this.href), i.get(this.href, {}, function(l) {
-					var o = i(l).find(".blog-post").length ? i(l) : i("<div></div>");
-					t.append(o.find(".blog-posts").html()), e.html(o.find("#Blog1_blog-pager-older-link").clone()), t.find(".more-article columns column-2").append(o.find(".more-article columns column-2 article").clone()), window.location.href.match("/search\\?updated-max") || (document.title = o.find(".article-header").first().text(), t.append('<div class="ignielMiddleAds">' + o.find(".ignielMiddleAds").html() + "</div>"), ignielMiddleAds()), n = !1
+			e.find("#Blog1_blog-pager-newer-link").remove(), e.on("click", "#Blog1_blog-pager-older-link", function() {
+				return history.pushState("", "", this.href), 
+				i.get(this.href, {}, function(l) {
+					var o = i(l).find(".blog-post").length ? i(l) 
+					: i("<div></div>");
+					t.append(o.find(".blog-posts").html()), 
+					e.html(o.find("#Blog1_blog-pager-older-link").clone()), 
+					t.find(".more-article columns column-2").append(o.find(".more-article columns column-2 article").clone()), window.location.href.match("/search\\?updated-max") || (document.title = o.find(".article-header").first().text(), n = !1
 				}, "html"), i(this).replaceWith('<span><img src="https://4.bp.blogspot.com/-a8y2WkWKzU0/W90DTo4X29I/AAAAAAAAG2c/5FWxJt9VaYUM7Mz-bH0emW3A50lJxCltQCLcBGAs/s1600/igniel-loading.gif"/></span>'), !1
 			}), config.auto && $(window).on("scroll resize", function() {
-				!n && $(this).scrollTop() + $(this).height() >= e.offset().top && (e.find("#Blog1_blog-pager-older-link a").trigger("click"), n = !0)
+				!n && $(this).scrollTop() + $(this).height() >= e.offset().top && (e.find("#Blog1_blog-pager-older-link").trigger("click"), n = !0)
 			})
 		}
 	}(jQuery)
