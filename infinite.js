1263d6f6911f2c8645531904f57c10725f27bfee
gaometri <37038820+gaometri@users.noreply.github.com>
2021-11-15 18:45:16 +0300

    Update infinite.js

--- infinite.js
+++ infinite.js
@@ -8,7 +8,7 @@ rain.async = !0, rain.onload = function() {
 				n = !1;
 			e.find("#Blog1_blog-pager-newer-link").remove(), e.on("click", "#Blog1_blog-pager-older-link a", function() {
 				return history.pushState("", "", this.href), i.get(this.href, {}, function(l) {
-					var o = i(l).find(".post-outer").length ? i(l) : i("<div></div>");
+					var o = i(l).find(".blog-post").length ? i(l) : i("<div></div>");
 					t.append(o.find(".blog-posts").html()), e.html(o.find("#Blog1_blog-pager-older-link").clone()), t.find(".more-article columns column-2").append(o.find(".more-article columns column-2 article").clone()), window.location.href.match("/search\\?updated-max") || (document.title = o.find(".article-header").first().text(), t.append('<div class="ignielMiddleAds">' + o.find(".ignielMiddleAds").html() + "</div>"), ignielMiddleAds()), n = !1
 				}, "html"), i(this).replaceWith('<span><img src="https://4.bp.blogspot.com/-a8y2WkWKzU0/W90DTo4X29I/AAAAAAAAG2c/5FWxJt9VaYUM7Mz-bH0emW3A50lJxCltQCLcBGAs/s1600/igniel-loading.gif"/></span>'), !1
 			}), config.auto && $(window).on("scroll resize", function() {
