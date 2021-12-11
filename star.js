545baee56d5df50af40fb4510bc2f69a95ed5fe5
gaometri <37038820+gaometri@users.noreply.github.com>
2021-12-11 19:10:30 +0300

    Create star.js

---
+++ star.js
@@ -0,0 +1,164 @@
+srs_0x4c1e = [" / ", "substring", "relative", "left", "starSize", "px)", "position", 'Firebase error. Add attribute firebaseURL="https://YOUR-FIREBASE.firebaseio.com" to your rating script.', "title", "getBoundingClientRect", "https://www.gstatic.com/firebasejs/8.1.1/firebase-app.js", "https://", "function", "none", "textAlign", "You have already cast your vote, your rating is $userRating$.", "contains", "appendChild", "bottomText", "0 0 0 -", "borderTop", "Firebase error", "'Comic Sans MS', cursive, sans-serif", "black", "1nhfBNZ", "'Palatino Linotype', 'Book Antiqua', Palatino, serif", "body", "Thanks for voting", 'script[src="https://www.gstatic.com/firebasejs/8.1.1/firebase-database.js"]', "textContent", "100%", 'script[src="https://cdn.jsdelivr.net/gh/starratingsystem/script@1.0.1/starrater.js"]', "transition", "oncontextmenu", "Verdana, Geneva, sans-serif", "Arial, Helvetica, sans-serif", "47143XogIoc", "zIndex", "blockingText", "'Lucida Sans Unicode', 'Lucida Grande', sans-serif", ".kozyr-SRS-average", "opacity", "transparent", "split", "initializeApp", "inline-block", "forEach", "full", "https://1.bp.blogspot.com/-pOr9XGwtSJc/Wsjf8ULOIqI/AAAAAAAAAKE/KBh-LUDIn0YzASKf-t7mQo8UNpdHhr2SgCLcBGAs/s1600/pusta.png", "display", "width", "getElementsByTagName", 'Firebase error. Enter the URL adress of your Firebase to "firebaseURL" attribute in your rating script.', "30639YMMkMx", "database", "0 0 0 0", "querySelectorAll", "173339trQLKh", "kozyr-SRS-loader", "dawaj 1s linear infinite", "white", "Firebase error. Invalid Fierabse URL", "firebaseio.com", "lineHeight", "firebaseURL", "val", "querySelector", "'Times New Roman', Times, serif", "6px solid #3498db", '"Lucida Sans Unicode", "Lucida Grande", sans-serif', "remove", "scrollX", "length", "class", '<span class="kozyr-SRS-average">0</span>', "25px", "insertBefore", "1.2", "fullStarImg", "'Arial Black', Gadget, sans-serif", "createElement", "calc(100% - ", "'Trebuchet MS', Helvetica, sans-serif", "hover", "undefined", "round", "Trebuchet", "src", "22055DWlruy", '"Trebuchet MS", Helvetica, sans-serif', "Palatino", "'Lucida Console', Monaco, monospace", "offsetWidth", "textColor", "'Courier New', Courier, monospace", "emptyStarImg", "161966UeYhRY", "https://www.gstatic.com/firebasejs/8.1.1/firebase-database.js", "empty", "div", "script", "value", "onclick", "hidden", "ratingName", "Lucida Console", "1px solid #e0e0e0", "align", "preventDefault", "bsrgl_", "object", "set", "head", "onmouseenter", "host", "https://3.bp.blogspot.com/-QSNdWP4Ijx4/Wsjf7QOUZ4I/AAAAAAAAAJ8/F2nReVG5WfA1rLV3dGcAFMsPOnIQck4YwCLcBGAs/s1600/pelna.png", "Arial Black", "Comic Sans", "200px", "fontSize", "Impact", "img", "animation", "1mZwkhr", "9999999", '"Lucida Console", Monaco, monospace', "163289RoWWHX", "boxSizing", "wartosc", "padding", "removeChild", "floor", "SRSstar", "borderRadius", "default", "thankYouText", "fontFamily", "other", "cursor", "Inherit", "6px solid #f3f3f3", "Times New Roman", "getAttribute", "parentNode", "height", "Average: <b>$average$</b> / $max$ (<b>$votes$</b> votes)", "setAttribute", "ref", "offsetHeight", "onmouseleave", "topText", "textSize", "Rating:", "maxWidth", "innerHTML", "numberOfStars", "indexOf", "border", "7px", "Tahoma", "inherit", "lastIndexOf", "status", "top", "50%", ".kozyr-SRS-votes", "background", "Verdana", "pointer", "scrollY", "style", '"Palatino Linotype", "Book Antiqua", Palatino, serif', "Tahoma, Geneva, sans-serif", "absolute", "auto", "right", "replace", "color", "overflow", "@keyframes dawaj {0% {transform: rotate(0deg);} 100% {transform:rotate(360deg);}} .kozyr-SRS-loader ~ div{visibility:hidden;}", "www.", "Impact, Charcoal, sans-serif", "___", '"Arial Black", Gadget, sans-serif', "margin", "57794AVuJQd", "2CrxsFw", "center"], srs_0x5d11 = function(e, t) {
+		return e -= 263, srs_0x4c1e[e]
+	}, srs_0x4fb41e = srs_0x5d11,
+	function(e, t) {
+		const a = srs_0x5d11;
+		for (;;) try {
+			if (141828 === parseInt(a(285)) + parseInt(a(414)) * parseInt(a(316)) + -parseInt(a(354)) + -parseInt(a(413)) + -parseInt(a(324)) * -parseInt(a(351)) + -parseInt(a(440)) * parseInt(a(264)) + parseInt(a(281))) break;
+			e.push(e.shift())
+		} catch (t) {
+			e.push(e.shift())
+		}
+	}(srs_0x4c1e), StarRatingGenerator = typeof StarRatingGenerator == srs_0x4fb41e(312) ? 0 : StarRatingGenerator + 1,
+	function(e) {
+		const t = srs_0x4fb41e;
+		if (0 === e && !document[t(294)](t(444))) {
+			let e = document[t(308)](t(328));
+			e[t(315)] = t(426), document[t(340)].appendChild(e), e.onload = function() {
+				const e = t;
+				let a = document.createElement(e(328));
+				a[e(315)] = e(325), document.head.appendChild(a)
+			};
+			let a = document[t(308)]("style");
+			a[t(382)] = t(407), document.head[t(433)](a)
+		}
+
+		function a(e, a, r) {
+			const s = t;
+			let o = e.getElementsByTagName(s(360));
+			for (let t = 0; t < o[s(300)]; t++) {
+				if (t <= a)
+					if (t < Math[s(359)](a)) o[t][s(294)](s(275))[s(398)][s(278)] = s(446), o[t].querySelector(s(326))[s(398)].width = "0%";
+					else {
+						let o = a - Math[s(359)](a),
+							n = e[s(279)](s(360))[t],
+							i = Math[s(313)](o * r);
+						n.querySelector(s(275))[s(398)][s(278)] = i + "px", n[s(294)](s(326))[s(398)][s(278)] = r - i + "px", n[s(294)](s(326))[s(294)](s(349))[s(398)].margin = s(435) + i + "px"
+					}
+				else t > a && (o[t][s(294)]("full")[s(398)][s(278)] = "0%", o[t][s(294)](s(326))[s(398)][s(278)] = s(446), o[t][s(294)](s(326))[s(294)](s(349))[s(398)][s(412)] = s(283));
+				o[t].querySelector(s(311)).style[s(278)] = "0%"
+			}
+		}
+
+		function r(e, a, r) {
+			const s = t;
+			let o = document[s(308)](s(327));
+			o[s(382)] = e[s(404)](/\$userRating\$/g, r), o[s(398)][s(422)] = s(401), o.style.background = s(439), o[s(398)][s(405)] = s(288), o[s(398)].border = s(334), o[s(398)][s(361)] = s(386), o[s(398)][s(357)] = "3px 7px", o[s(398)][s(291)] = s(305), o[s(398)][s(430)] = s(415), o.style[s(269)] = "0", o.style[s(448)] = "opacity 1s", o[s(398)][s(278)] = s(346), o[s(398)][s(355)] = "border-box", o[s(398)][s(265)] = s(352), o[s(398)][s(364)] = s(441), document[s(442)].appendChild(o);
+			let n = a[s(425)]();
+			setTimeout(function() {
+				const e = s;
+				o.style[e(269)] = "1", "right" === a[e(398)][e(430)] ? o[e(398)][e(419)] = window[e(299)] + n[e(419)] + a[e(320)] - 200 + "px" : a[e(398)][e(430)] === e(415) ? o.style[e(419)] = window[e(299)] + n.left + a[e(320)] / 2 - 100 + "px" : o.style.left = window[e(299)] + n[e(419)] + "px", o[e(398)][e(391)] = (n.top > o[e(376)] ? window[e(397)] + n[e(391)] - o[e(376)] : window[e(397)] + n[e(391)] + o[e(376)]) + "px"
+			}, 10), setTimeout(function() {
+				const e = s;
+				o.style[e(269)] = "0", setTimeout(function() {
+					const t = e;
+					document[t(442)][t(358)](o)
+				}, 1e3)
+			}, 3500)
+		}! function s(o) {
+			const n = t;
+			typeof firebase == n(338) && typeof firebase[n(282)] == n(428) && "function" == typeof firebase[n(272)] ? function(s) {
+				const o = t;
+				let n = document[o(284)](o(447))[s],
+					i = location[o(342)].replace(o(408), "").replace(/\./g, "_")[o(404)](/\//g, "__");
+				"" === i && (i = o(365));
+				let l = n[o(370)](o(332));
+				if (!l || l === o(402)) {
+					let e = location.href[o(271)]("?")[0].split("#")[0].replace(location.protocol + "//", "")[o(404)](o(408), "");
+					("/" === e[o(417)](e[o(300)] - 1) || "." === e[o(417)](e[o(300)] - 1)) && (e = e.substring(0, e[o(300)] - 1)), e = e[o(404)](/\./g, "_")[o(404)](/\//g, "__")[o(404)](/\,/g, o(410))[o(404)](/\s/g, ""), l = e
+				}
+				l = l[o(404)](/\s/g, "_").replace(/\#/g, "-").replace(/\./g, "-").replace(/\@/g, "-")[o(404)](/\!/g, "-")[o(404)](/\$/g, "-")[o(404)](/\%/g, "-").replace(/\&/g, "-")[o(404)](/\(/g, "-").replace(/\)/g, "-");
+				let c = n.getAttribute(o(323)) ? n[o(370)]("emptyStarImg") : o(276),
+					d = n[o(370)](o(306)) ? n[o(370)](o(306)) : o(343),
+					u = n[o(370)]("hoverStarImg");
+				null !== u && "" != u || (u = d);
+				let p = n[o(370)](o(420));
+				(!p || Number(p) < 0 || isNaN(p)) && (p = 25), p = Number(p);
+				let f = n.getAttribute(o(266));
+				null === f && (f = o(431));
+				let g = n[o(370)](o(335));
+				g !== o(403) && g !== o(419) && (g = o(415));
+				let m = n[o(370)](o(379));
+				(!m || Number(m) < 0 || isNaN(m)) && (m = 15), m = Number(m);
+				let b = n[o(370)](o(321)) ? n[o(370)](o(321)) : o(388),
+					h = n.getAttribute("fontFamily");
+				h = "Georgia" == h || "Georgia, serif" == h ? "Georgia, serif" : h == o(318) || h == o(441) ? o(399) : h == o(369) || h == o(295) ? '"Times New Roman", Times, serif' : "Arial" == h || h == o(263) ? o(263) : h == o(344) || h == o(307) ? o(411) : h == o(345) || h == o(438) ? '"Comic Sans MS", cursive, sans-serif' : h == o(348) || "Impact, Charcoal, sans-serif" == h ? o(409) : "Lucida Sans" == h || h == o(267) ? o(297) : h == o(387) || h == o(400) ? o(400) : h == o(314) || h == o(310) ? o(317) : h == o(395) || h == o(450) ? o(450) : "Courier New" == h || h == o(322) ? '"Courier New", Courier, monospace' : h == o(333) || h == o(319) ? o(353) : o(367);
+				let y = "readonly" === n[o(370)](o(390)),
+					S = null !== n[o(370)](o(378)) ? n[o(370)](o(378)) : o(380),
+					x = null !== n[o(370)](o(434)) ? n[o(370)](o(434)) : o(373),
+					A = Number(n.getAttribute(o(383)));
+				(A < 1 || isNaN(A)) && (A = 5);
+				let w = null !== n[o(370)](o(363)) ? n[o(370)](o(363)) : o(443),
+					v = document[o(308)]("div");
+				v[o(374)](o(332), l), n[o(371)][o(304)](v, n), v.style[o(430)] = g, v[o(398)][o(422)] = o(418), v[o(449)] = function(e) {
+					return e[o(336)](), !1
+				};
+				let R = document[o(308)](o(327));
+				R[o(374)](o(301), o(286)), R[o(398)][o(385)] = o(368), R[o(398)].borderRadius = o(392), R[o(398)][o(436)] = o(296), R[o(398)].width = o(303), R[o(398)][o(372)] = o(303), R.style[o(350)] = o(287), "center" === g ? R[o(398)][o(412)] = o(402) : g === o(403) && (R.style.marginLeft = o(309) + p * A + o(421)), v[o(433)](R);
+				let I = document[o(308)](o(327)),
+					O = document[o(308)](o(327));
+				x = x[o(404)](/\$average\$/g, o(302))[o(404)](/\$votes\$/g, '<span class="kozyr-SRS-votes">0</span>')[o(404)](/\$max\$/g, A), I[o(382)] = S, I[o(398)][o(347)] = m + "px", I[o(398)][o(291)] = "1.2", I[o(398)][o(364)] = h, I[o(398)][o(430)] = g, I.style.color = b, v[o(433)](I), O[o(398)].fontSize = m + "px", O[o(398)].lineHeight = o(305), O[o(398)][o(364)] = h, O[o(398)][o(430)] = g, O.style[o(405)] = b, O[o(382)] = x;
+				let k = n.getAttribute(o(292));
+				null === k ? k = o(423) : "" == k ? k = o(280) : 0 !== k[o(384)](o(427)) ? k = o(289) : k[o(389)](o(290)) < 5 ? k = o(289) : k[o(389)]("/") !== k.length - 1 && (k += "/");
+				let C = document[o(308)]("div");
+				C[o(398)][o(278)] = p * A + "px", C[o(398)][o(277)] = o(273), v[o(433)](C);
+				for (let e = 1; e <= A; e++) {
+					let t = document[o(308)](o(360));
+					t[o(398)][o(277)] = o(273), t[o(398)].width = p + "px", t.style[o(366)] = localStorage[o(337) + l] || y ? o(362) : o(396), !y && (t[o(341)] = function() {
+						const t = o;
+						if (localStorage[t(337) + l]) v[t(424)] = f[t(404)](/\$userRating\$/g, localStorage[t(337) + l]), v[t(398)][t(366)] = t(362);
+						else {
+							let a = v[t(279)](t(360));
+							for (let r = 0; r < a[t(300)]; r++) r < e ? (a[r][t(294)](t(275)).style[t(278)] = "0%", a[r][t(294)](t(326))[t(398)][t(278)] = "0%", a[r][t(294)](t(311))[t(398)][t(278)] = t(446)) : (a[r][t(294)](t(275)).style[t(278)] = "0%", a[r][t(294)](t(311)).style[t(278)] = "0%", a[r][t(294)]("empty")[t(398)][t(278)] = t(446), a[r][t(294)]("empty")[t(294)]("img")[t(398)][t(412)] = "0 0 0 0");
+							"" != S && (I[t(382)] = e + t(416) + A)
+						}
+					}), t[o(374)](o(356), e);
+					let a = document[o(308)](o(275));
+					a[o(398)][o(277)] = "inline-block", a[o(398)][o(406)] = o(331);
+					let r = document[o(308)](o(326));
+					r[o(398)][o(277)] = o(273), r[o(398)].overflow = o(331);
+					let s = document[o(308)](o(311));
+					s[o(398)][o(277)] = o(273), s[o(398)][o(406)] = o(331);
+					let n = document.createElement("img");
+					n.style[o(394)] = o(270), n[o(398)][o(385)] = "0", n[o(398)][o(357)] = "0", n[o(398)][o(412)] = "0", n.style.maxWidth = o(429);
+					let i = document[o(308)](o(349));
+					i[o(398)][o(394)] = o(270), i.style[o(385)] = "0", i[o(398)].padding = "0", i[o(398)][o(412)] = "0", i[o(398)][o(381)] = o(429);
+					let g = document[o(308)](o(349));
+					g[o(398)][o(394)] = o(270), g[o(398)][o(385)] = "0", g[o(398)].padding = "0", g[o(398)].margin = "0", g[o(398)][o(381)] = o(429), n[o(315)] = c, i[o(315)] = d, g[o(315)] = u, n.style.width = p + "px", i.style[o(278)] = p + "px", g.style[o(278)] = p + "px", t[o(398)][o(291)] = "0", a[o(433)](i), r[o(433)](n), s[o(433)](g), t[o(433)](a), t[o(433)](r), t.appendChild(s), C[o(433)](t)
+				}
+				if (v[o(433)](O), a(v, 0, p), k[o(384)](o(437)) < 0) {
+					const t = {};
+					t.databaseURL = k;
+					let s = firebase.initializeApp(t, l + e)[o(282)]()[o(375)]("StarRatingSystem/" + i + "/" + l);
+					s.on(o(329), e => {
+						const t = o;
+						let n = e[t(293)]();
+						if (!n) {
+							const e = {
+								OO: 0,
+								O0: 0
+							};
+							n = e
+						}
+						a(v, n.OO * A, p), v[t(432)](R) && R[t(298)](), C[t(377)] = function() {
+							const e = t;
+							a(v, n.OO * A, p), I[e(382)] = S
+						}, v[t(284)](t(268)).forEach(e => e[t(445)] = Math[t(313)](n.OO * A * 100) / 100), v[t(284)](t(393))[t(274)](e => e[t(445)] = n.O0), !y && v.querySelectorAll(t(360))[t(274)]((e, a) => {
+							const o = t;
+							e[o(330)] = function() {
+								const e = o;
+								if (localStorage[e(337) + l]) r(f, v, localStorage[e(337) + l]);
+								else {
+									var t = (n.OO * n.O0 + (a + 1) / A) / (n.O0 + 1);
+									const o = {};
+									o.OO = t, o.O0 = n.O0 + 1, s[e(339)](o), localStorage[e(337) + l] = a + 1, v[e(284)](e(360)).forEach(t => t[e(398)][e(366)] = e(362)), r(w, v, a + 1), I[e(382)] = S
+								}
+							}
+						})
+					})
+				} else v[o(382)] = k
+			}(o) : setTimeout(function() {
+				s(o)
+			}, 50)
+		}(e)
+	}(StarRatingGenerator);
